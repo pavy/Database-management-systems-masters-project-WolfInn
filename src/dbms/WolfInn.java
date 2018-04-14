72a43d7f687e2118afb4c0f82ea39066cb4318fe
saraswatimishra <saraswatimishra18@gmail.com>
2018-04-13 20:07:23 -0400

    Code Merge

--- src/dbms/WolfInn.java
+++ src/dbms/WolfInn.java
@@ -1,11 +1,13 @@
 package dbms;
-import java.sql.* ;  // for standard JDBC programs
+
+import java.sql.*; // for standard JDBC programs
 import java.util.Scanner;
 
 public class WolfInn {
-	
-	// Update user info 
-	private static final String jdbcURL = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/piyer3"; // Using SERVICE_NAME
+
+	// Update user info
+	private static final String jdbcURL = "jdbc:mariadb://classdb2.csc.ncsu.edu:3306/piyer3"; // Using
+																								// SERVICE_NAME
 
 	// Update your user and password info here!
 
@@ -15,68 +17,113 @@ public class WolfInn {
 	public static void main(String[] args) {
 		try {
 			// Loading the driver. This creates an instance of the driver
-			// and calls the registerDriver method to make MySql(MariaDB) Thin available to clients.
+			// and calls the registerDriver method to make MySql(MariaDB) Thin
+			// available to clients.
 			Class.forName("org.mariadb.jdbc.Driver");
-
 			Connection connection = null;
-            Statement statement = null;
-            ResultSet result = null;
+			Statement statement = null;
 
-            try {
-            // Get a connection instance from the first driver in the
-            // DriverManager list that recognizes the URL jdbcURL
-            connection = DriverManager.getConnection(jdbcURL, user, password);
+			try {
+				// Get a connection instance from the first driver in the
+				// DriverManager list that recognizes the URL jdbcURL
+				connection = DriverManager.getConnection(jdbcURL, user,
+						password);
 
-            // Create a statement instance that will be sending
-            // your SQL statements to the DBMS
-            statement = connection.createStatement();
+				// Create a statement instance that will be sending
+				// your SQL statements to the DBMS
+				statement = connection.createStatement();
 
-            // use statement to write create/update statements
-            //statement.executeUpdate("CREATE TABLE CATS (CNAME VARCHAR(20), " +
-            //"TYPE VARCHAR(30), AGE INTEGER, WEIGHT FLOAT, SEX CHAR(1))");
-            Scanner scan = new Scanner(System.in);
-    		
-            System.out.println("Please select the Task or Operation you would like to perform: ");
-            System.out.println("-------------------------\n");
-            System.out.println("1 - Information Processing");
-            System.out.println("2 - Maintaining Service Records");
-            System.out.println("3 - Maintaining billing accounts");
-            System.out.println("4 - Reports");
+				// use statement to write create/update statements
+				// statement.executeUpdate("CREATE TABLE CATS (CNAME VARCHAR(20), "
+				// +
+				// "TYPE VARCHAR(30), AGE INTEGER, WEIGHT FLOAT, SEX CHAR(1))");
+				mainMenu(statement, connection);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-            int choice = scan.nextInt();
-            HotelUtility obj = new HotelUtility();
-            switch(choice) {
-            		case 1 :	infoProcessing(obj, statement);
-            					break;
-            		case 2 : obj.maintainServiceRecord(statement);
-            					break;
-            		case 3 : obj.maintainBillingAccount(statement);
-    				break;
-            		case 4 : generateReport();
-    				break;
-            		default : System.out.println("Please select a valid task or operation");
-    				break;
-            }
-            scan.close();
-            
-            } finally {
-                close(result);
-                close(statement);
-                close(connection);
-            }
-		} catch(Throwable oops) {
-            oops.printStackTrace();
-        }
 	}
 
-	private static void generateReport() {
+	private static void generateReport(Statement statement, Connection connection) {
 		// TODO Auto-generated method stub
+
+		System.out.println("1 - Report occupancy by hotel");
+		System.out.println("2 - Report occupancy by room type");
+		System.out.println("3 - Report occupancy by date range");
+		System.out.println("4 - Report occupancy by date City");
+		System.out.println("5 - Report of staff information group by role");
+		System.out
+				.println("6 - Information on all staff members serving the customer during the stay");
+		System.out
+				.println("7 - Revenue earned by a hotel during a given date range");
+		System.out.println("8 - Return to main menu");
+		HotelUtility obj = new HotelUtility();
+		Scanner scan = new Scanner(System.in);
+		int choice = scan.nextInt();
+		switch (choice) {
+		case 1:
+			obj.reportOccupancyByHotel(statement);
+			break;
+
+		case 2:
+			obj.reportOccupancyByRoomType(statement);
+			break;
+
+		case 3:
+			obj.reportOccupancyByDateRange(statement);
+			break;
+
+		case 4:
+			obj.reportOccupancyByCity(statement);
+			break;
+
+		case 5:
+			obj.reportStaffInfoGroupedByRole(statement);
+			break;
+
+		case 6:
+			obj.reportStaffServingCustomerDuringStay(statement);
+			break;
+
+		case 7:
+			obj.reportRevenueEarnedInDateRange(statement);
+			break;
+			
+		case 8: mainMenu(statement, connection);
+
+		default:
+			System.out.println("Not a valid choice");
+			break;
+		}
 		
+		System.out.println("Do you want to continue?(Y/N)");
+		Scanner sc = new Scanner(System.in);
+		String nextChoice = scan.nextLine();
+		while(!(nextChoice.equals("Y")||nextChoice.equals("y")||nextChoice.equals("N")||nextChoice.equals("n")))
+			nextChoice = scan.nextLine();
+		if(!nextChoice.equals("Y")||!nextChoice.equals("y"))
+			generateReport(statement, connection);
+		else 
+			mainMenu(statement, connection);
 	}
 
-	private static void infoProcessing(HotelUtility obj, Statement statement) throws SQLException {
+	private static void closeConnection(Statement statement, Connection connection) {
+		try {
+			close(statement);
+			close(connection);
+			System.out.println("Closing all connections and exiting from the system...");
+			System.exit(0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void infoProcessing( Statement statement, Connection connection) throws SQLException {
 		// handle 15 cases
-		
+		HotelUtility obj = new HotelUtility();
 		Scanner scan = new Scanner(System.in);
 
         System.out.println("1 - Enter information about Hotel");
@@ -134,28 +181,119 @@ public class WolfInn {
         		default : System.out.println("Please select a valid task");
         					break;
         }
-        scan.close();
+        
+        System.out.println("Do you want to continue?(Y/N)");
+		
+		String nextChoice = scan.nextLine();
+		while(!(nextChoice.equals("Y")||nextChoice.equals("y")||nextChoice.equals("N")||nextChoice.equals("n")))
+			nextChoice = scan.nextLine();
+		if(!nextChoice.equals("Y")||!nextChoice.equals("y"))
+			generateReport(statement, connection);
+		else 
+			mainMenu(statement, connection);
+		
 	}
-	
+	/*private static void infoProcessing() {
+		// handle 15 cases
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("1 - Enter information about Hotel");
+		System.out.println("2 - Update Hotel information");
+		System.out.println("3 - Delete Hotel");
+		System.out.println("4 - Enter information about Rooms");
+		System.out.println("5 - Update Room information");
+		System.out.println("6 - Delete Room");
+		System.out.println("7 - Enter information about Staff");
+		System.out.println("8 - Update Staff information");
+		System.out.println("9 - Delete Staff");
+		System.out.println("10 - Enter information about Customer");
+		System.out.println("11 - Update Customer information");
+		System.out.println("12 - Delete Customer");
+		System.out.println("13 - Check Availability of room");
+		System.out.println("14 - Assign room to customer");
+		System.out.println("15 - Release room");
+
+		int choice = scan.nextInt();
+
+		switch (choice) {
+		case 1: // add respective function calls
+			break;
+		default:
+			System.out.println("Please select a valid task");
+			break;
+		}
+		scan.close();
+	}*/
+
 	static void close(Connection connection) {
-        if(connection != null) {
-            try {
-            connection.close();
-            } catch(Throwable whatever) {}
-        }
-    }
-    static void close(Statement statement) {
-        if(statement != null) {
-            try {
-            statement.close();
-            } catch(Throwable whatever) {}
-        }
-    }
-    static void close(ResultSet result) {
-        if(result != null) {
-            try {
-            result.close();
-            } catch(Throwable whatever) {}
-        }
-    }
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (Throwable whatever) {
+			}
+		}
+	}
+
+	static void close(Statement statement) {
+		if (statement != null) {
+			try {
+				statement.close();
+			} catch (Throwable whatever) {
+			}
+		}
+	}
+
+	static void close(ResultSet result) {
+		if (result != null) {
+			try {
+				result.close();
+			} catch (Throwable whatever) {
+			}
+		}
+	}
+
+	private static void mainMenu(Statement statement, Connection connection) {
+		try {
+
+			Scanner scan = new Scanner(System.in);
+
+			System.out
+					.println("Please select the Task or Operation you would like to perform: ");
+			System.out.println("-------------------------\n");
+			System.out.println("1 - Information Processing");
+			System.out.println("2 - Maintaining Service Records");
+			System.out.println("3 - Maintaining billing accounts");
+			System.out.println("4 - Reports");
+			System.out.println("5 - Close application");
+
+			int choice = scan.nextInt();
+			HotelUtility obj = new HotelUtility();
+			switch (choice) {
+			case 1:
+				infoProcessing(statement, connection);
+				break;
+			case 2:
+				obj.maintainServiceRecord(statement);
+				break;
+			case 3:
+				obj.maintainBillingAccount(statement);
+				break;
+			case 4:
+				generateReport(statement, connection);
+				break;
+			case 5: closeConnection(statement, connection);
+				break;
+			default:
+				System.out.println("Incorrect value entered. Closing application...");
+				break;
+			}
+			scan.close();
+
+		}
+
+		catch (Throwable oops) {
+			oops.printStackTrace();
+		}
+	}
 }
+
